30dcc958a2bfa01dca3f21d96f2988ad94914cce
syahputrawork98-sketch <syah.putrawork98@gmail.com>
2026-06-20 04:23:13 +0700

    Add flash sale product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::updateOrCreate(
+            ['name' => 'Flash Sale Product'],
+            [
+                'price' => 50000,
+                'stock' => 10,
+            ]
+        );
+    }
+}
